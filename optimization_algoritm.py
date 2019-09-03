d1561d4943fb176a2e37dc4028c47687eacaff4e
Bugli Lorenzo <bugli.lorenzo@gmail.com>
2019-09-03 20:25:34 +0200

    Optimization algoritm variant not working

--- optimization_algoritm.py
+++ optimization_algoritm.py
@@ -1,6 +1,7 @@
 def optimize1(Y, P, fn, eps=0.01, ):
-    S = 0.1
     d = lambda x, y: abs(x - y)
+
+    S = 100
     z = 1  # last is increment
     c = 0
     while d(fn(P), Y) > eps:
@@ -21,22 +22,58 @@ def optimize1(Y, P, fn, eps=0.01, ):
 
 
 def optimize2(Y, P, fn, eps=0.01, ):
-    pass
+    d = lambda x, y: abs(x - y)
+
+    S = 100
+    c = 0
+    old_distance = 10000
+    STEP = 100
+
+    progress = True
+    while d(fn(P), Y) > eps and progress:
+        c += 1
+        P += S
+        current_distance = d(fn(P), Y)
+
+        if current_distance > old_distance:
+            S = -STEP
+            STEP /= c
+
+        progress = d(old_distance, current_distance)
+        old_distance = current_distance
+
+    return c, round(P, 3)
 
 
 if __name__ == '__main__':
 
-    function_list = [
-        lambda x: x ** 2,
-        lambda x: x * 2,
-        lambda x: x * 0.0031,
-    ]
+    def run_optiomize(algoritm):
+        TPL = "{:>4}{:>15}{:>15}{:>15}"
 
-    for fn in function_list:
-        iterations, result = optimize1(
-            Y=61,
-            P=2,
-            fn=fn
-        )
+        print(TPL.format("index", "iter", "result", "fn(result)"))
 
-        print(iterations, result, fn(result))
+        function_list = {
+            1: lambda x: x ** 2,
+            2: lambda x: x * 2,
+            3: lambda x: x * 0.0031,
+        }
+
+        for index, fn in function_list.items():
+            iterations, result = algoritm(
+                Y=61,
+                P=2,
+                fn=fn
+            )
+
+            print(TPL.format(
+                index,
+                iterations,
+                result,
+                round(fn(result), 6)
+            ))
+
+        print("\n\n")
+
+
+    run_optiomize(optimize1)
+    run_optiomize(optimize2)
