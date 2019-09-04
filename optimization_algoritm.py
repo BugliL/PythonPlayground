ecf22b9c37c7529e5ab1048233ae8583e02d780c
Bugli Lorenzo <bugli.lorenzo@gmail.com>
2019-09-04 11:26:28 +0200

    Added secants method

--- optimization_algoritm.py
+++ optimization_algoritm.py
@@ -1,3 +1,6 @@
+import math
+
+
 def optimize1(Y, P, fn, eps=0.01, ):
     d = lambda x, y: abs(x - y)
 
@@ -44,6 +47,26 @@ def optimize2(Y, P, fn, eps=0.01, ):
     return c, round(P, 3)
 
 
+def secants(Y, P, fn, eps=0.01, ):
+    d = lambda x, y: abs(x - y)
+    fn_ = lambda x: Y - fn(x)
+
+    value = P*0.1
+    x0, x1 = P - value, P + value
+    x = x0
+    den = (fn_(x1) - fn_(x0))
+    c = 0
+
+    while d(fn_(x), Y) >= eps and abs(den) > eps:  # and is_iterating(x, x1):
+        x = x1 - fn_(x1) * (x1 - x0) / den
+        x0, x1 = x1, x
+
+        c += 1
+        den = (fn_(x1) - fn_(x0))
+
+    return c, round(x, 3)
+
+
 if __name__ == '__main__':
 
     def run_optiomize(algoritm):
@@ -55,24 +78,38 @@ if __name__ == '__main__':
             1: lambda x: x ** 2,
             2: lambda x: x * 2,
             3: lambda x: x * 0.0031,
+            4: lambda x: x ** 3,
+            5: lambda x: 4 * (x ** (1 / 3)),
+            # 6: lambda x: math.log(x),
+            # 7: lambda x: math.exp(x),
         }
 
         for index, fn in function_list.items():
-            iterations, result = algoritm(
-                Y=61,
-                P=2,
-                fn=fn
-            )
-
-            print(TPL.format(
-                index,
-                iterations,
-                result,
-                round(fn(result), 6)
-            ))
+            try:
+                iterations, result = algoritm(
+                    Y=127,
+                    P=20,
+                    fn=fn
+                )
+            except Exception:
+                error_string = 'ERR'
+                print(TPL.format(index, error_string, error_string, error_string))
+            else:
+                print(TPL.format(
+                    index,
+                    iterations,
+                    result,
+                    round(fn(result), 6)
+                ))
 
         print("\n\n")
 
 
+    print("First")
     run_optiomize(optimize1)
+
+    print("Second")
     run_optiomize(optimize2)
+
+    print("Secants")
+    run_optiomize(secants)
